3ed1045b5442bdc091721cbedce91bd9b2678047
kate <youremail@aol.com>
2021-08-09 10:49:25 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './styles/App.scss';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hi11</h1>
+    </div>
+  );
+}
+
+export default App;
